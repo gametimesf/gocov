f08af2f92930d1cc032499be5aea9c8a88dc617d
iwaesc <60084825+iwaesc@users.noreply.github.com>
2025-10-22 12:59:36 -0400

    refactor: update repository URLs in code and documentation

--- gocov/main.go
+++ gocov/main.go
@@ -27,8 +27,8 @@ import (
 	"io"
 	"os"
 
-	"github.com/axw/gocov"
-	"github.com/axw/gocov/gocov/convert"
+	"github.com/gametimesf/gocov"
+	"github.com/gametimesf/gocov/gocov/convert"
 )
 
 func usage() {
